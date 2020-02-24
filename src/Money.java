7d60e75ab57f2fa09b30f2e9b96f7a88f11a53fe
silverwhole <silverwhole@hotmail.com>
2020-02-24 15:50:32 +0630

    Initial commit for Money class

---
+++ src/Money.java
@@ -0,0 +1,31 @@
+import org.jetbrains.annotations.NotNull;
+
+/*Representation for money amounts
+* */
+public class Money {
+    private int dollar;
+
+    public Money(int dollar){
+        this.setDollar(dollar);
+    }
+
+    public String toString(){
+        return "$" + this.getDollar();
+    }
+
+    public void add(@NotNull Money money){
+        this.setDollar(this.getDollar() + money.getDollar());
+    }
+
+    public void subtract(@NotNull Money money){
+        this.setDollar(this.getDollar() - money.getDollar());
+    }
+
+    public int getDollar() {
+        return dollar;
+    }
+
+    public void setDollar(int dollar) {
+        this.dollar = dollar;
+    }
+}
